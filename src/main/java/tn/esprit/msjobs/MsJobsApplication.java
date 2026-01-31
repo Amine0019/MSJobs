e21dc515a4289703d5d2a4a86ec1ae7b7f5dfd9a
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2026-01-31 17:24:42 +0100

    FirstCommit

---
+++ src/main/java/tn/esprit/msjobs/MsJobsApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.msjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsJobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/msjobs/MsJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.msjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
