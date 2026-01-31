5904555969494c5b47930509f2a6bf0919c13bac
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2026-01-31 17:53:47 +0100

    la méthode application runner qui permet de ajouter automatiquement des Jobs Dans la base de données

--- src/main/java/tn/esprit/msjobs/MsJobsApplication.java
+++ src/main/java/tn/esprit/msjobs/MsJobsApplication.java
@@ -1,7 +1,12 @@
 package tn.esprit.msjobs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tn.esprit.msjobs.Entities.Jobs;
+import tn.esprit.msjobs.Repositories.IJobRepository;
 
 @SpringBootApplication
 public class MsJobsApplication {
@@ -10,4 +15,23 @@ public class MsJobsApplication {
         SpringApplication.run(MsJobsApplication.class, args);
     }
 
+    @Autowired
+    private IJobRepository jobRepository;
+
+    @Bean
+    ApplicationRunner init() {
+        return (args) -> {
+
+            jobRepository.save(new Jobs(true,"Service Financier"));
+            jobRepository.save(new Jobs(false,"Service Info" ));
+            jobRepository.save(new Jobs(false,"Service RH"));
+
+            jobRepository.findAll().forEach(System.out::println);
+        };
+    }
+
 }
+
+
+
+
